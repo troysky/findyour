7aef90ce084de636f563c6d544c580db2af1b1a4
jnd <skybluezone@gmail.com>
2014-07-20 08:43:36 +1000

    transfer funds init checkin

--- MailProcessor.js
+++ MailProcessor.js
@@ -3,82 +3,139 @@ var mandrill_client = new mandrill.Mandrill('K5PMmRLm9JJ6z56JemR9UA');
 var jsdom = require('jsdom').jsdom;
 var jquery = require('jquery');
 var request = require('request');
+var dbm = require('./dbManager');
 var q = require('q');
+var postCodeSearchCategory = [];
+var PROS_COL = "professionals";
 
 exports.process = function(mail){
-	if(mail.subject.indexOf("New submission") < 0){
-		console.log("mail subject does not match: New submission*");
-		return;
-	}
-
 	if(!mail.html){
 		console.log("failed to process mail: no HTML contents found");
-		return;
 	}
 
-	console.log("start processing mail: " + mail.subject);
+	if(mail.subject.indexOf("New submission") >= 0){
+		this.processNewJob(mail);
+	}else if(mail.subject.indexOf("New Registration") >= 0){
+		this.processNewRegistration(mail);
+	}else{
+		console.log("mail subject does not match: (New submission|New Registration)*");		
+	}
+}
 
+exports.processNewJob = function(mail){
+	console.log("start processing new job: " + mail.subject);
 	this.extractCriteria(mail.html)
 	.then(this.findNearByPostCodes, this.handleError)
 	.then(this.findPros, this.handleError)
 	.then(this.distribute.bind(this), this.handleError);
 }
 
+exports.processNewRegistration = function(mail){
+	console.log("start processing new registration: " + mail.subject);
+	this.extractDetails(mail.html)
+	.then(this.saveRegistration, this.handleError)
+	.then(function(doc){console.log("details saved", doc)}, this.handleError);
+}
+
 exports.extractCriteria = function(html){
 	var deferred = q.defer();
-	console.log("extracting service type and postcode");
-	try{
-		var window = jsdom("<html><body>" + html + "</body></html>", null, {
-		        // standard options:  disable loading other assets
-		        // or executing script tags
-		        FetchExternalResources: false,
-		        ProcessExternalResources: false,
-		        MutationEvents: false,
-		        QuerySelector: false
-		}).parentWindow;
-		jsdom.jQueryify(window, jquery, function () {
+	console.log("extracting criteria");
+	var window = jsdom("<html><body>" + html + "</body></html>", null, {
+	        // standard options:  disable loading other assets
+	        // or executing script tags
+	        FetchExternalResources: false,
+	        ProcessExternalResources: false,
+	        MutationEvents: false,
+	        QuerySelector: false
+	}).parentWindow;
+	jsdom.jQueryify(window, jquery, function () {
+		try{
+			debugger;
 			var criteria = {
 				customerRequest: html,
-				//serviceType: window.$('tr:has(td:contains("Choose your service"))').next().find('td').text().trim(),
-				postCode: window.$('tr:has(td:contains("Post Code"))').next().find('td').text().trim().substring(0,4)
+				category: window.$('tr:has(td:contains("Category"))').eq(1).next().find('td').text().trim(),
+				postCode: window.$('tr:has(td:contains("Post Code"))').eq(1).next().find('td').text().trim().substring(0,4)
 			}
 			deferred.resolve(criteria);
-		});
-	}catch(e){
-		deferred.reject("failed to extract criteria: " + e);
-	}
+		}catch(e){
+			deferred.reject("failed to extract criteria: " + e);
+		}
+	});	
 
 	return deferred.promise;
+}
+
+exports.extractDetails = function(html){
+	var deferred = q.defer();
+	console.log("extracting pros detail");
+	var window = jsdom("<html><body>" + html + "</body></html>", null, {
+	        // standard options:  disable loading other assets
+	        // or executing script tags
+	        FetchExternalResources: false,
+	        ProcessExternalResources: false,
+	        MutationEvents: false,
+	        QuerySelector: false
+	}).parentWindow;
+	jsdom.jQueryify(window, jquery, function () {
+		try{
+			/*var companyName = window.$('tr:has(td:contains("Company Name"))').next().find('td').text().trim();
+			var details = {				
+				_id: companyName,
+				companyName: companyName,
+				category: window.$('tr:has(td:contains("Category"))').next().find('td').text().trim(),
+				email: window.$('tr:has(td:contains("Email"))').next().find('td').text().trim().substring(0,4)
+			}*/
+			var details = {				
+				_id: "Test Trader",
+				companyName: "Test Trader",
+				category: "Transfer Funds",
+				email: "support@findyour.com.au"
+			}
+			deferred.resolve(details);
+		}catch(e){
+			deferred.reject("failed to parse contents: " + e);
+		}
+	});
+
+	return deferred.promise;
+}
+
+exports.saveRegistration = function(details){
+	console.log("saving pros detail", details);
+	return dbm.save(PROS_COL, details);
 }
 
 exports.findNearByPostCodes = function(criteria){	
 	var deferred = q.defer();
-	console.log("searching nearby postcodes for " + criteria.postCode);
-	request('http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=' + criteria.postCode + '&country=AU&radius=5&username=jndsolution&maxRows=500', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var jsonBody = JSON.parse(body);
-			var array = jsonBody.postalCodes;
-			var postCodes = [];
-			for(var i = array.length-1; i >= 0; i--){
-				var pc = array[i].postalCode;
-				if(pc.slice(0, 1) !== "1"){
-					postCodes.push(pc);
+	if(criteria.postCode && postCodeSearchCategory.indexOf(criteria.category) >= 0){
+		console.log("searching nearby postcodes for " + criteria.postCode);
+		request('http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=' + criteria.postCode + '&country=AU&radius=5&username=jndsolution&maxRows=500', function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var jsonBody = JSON.parse(body);
+				var array = jsonBody.postalCodes;
+				var postCodes = [];
+				for(var i = array.length-1; i >= 0; i--){
+					var pc = array[i].postalCode;
+					if(pc.slice(0, 1) !== "1"){
+						postCodes.push(pc);
+					}
 				}
+				criteria.postCodes = postCodes;
+				deferred.resolve(criteria);
+			}else{
+				deferred.reject("failed to extract nearby postcodes: " + error);
 			}
-			criteria.postCodes = postCodes;
-			deferred.resolve(criteria);
-		}else{
-			deferred.reject("failed to extract nearby postcodes: " + error);
-		}
-	})
+		});
+	}else{
+		deferred.resolve(criteria);
+	}
 	return deferred.promise;
 }
 
 exports.findPros = function(criteria){
-	//console.log("searching pros for " + criteria.serviceType + " in " + criteria.postCodes);
-	console.log("searching pros in " + criteria.postCodes);
-	criteria.pros = ["jndsolutiontest@gmail.com"];
-	return criteria;
+	console.log("searching pros for ", criteria.category);	
+	//criteria.pros = ["jndsolutiontest@gmail.com"];
+	return dbm.find(PROS_COL, {category: criteria.category}, criteria);
 }
 
 exports.handleError = function(reason){
@@ -86,10 +143,12 @@ exports.handleError = function(reason){
 }
 
 exports.distribute = function(details){	
-	var htmlReply = "<p>nearby postcodes</p><div>" + details.postCodes + "</div><p>customer original request</p><div> " + details.customerRequest + " </div>";
-	for(var i = details.pros.length -1; i >= 0; i--){
-		var recipient = details.pros[i];		
-		var message = this.createMessage("system@findyour.com.au", "customer quote request -test", recipient, htmlReply);
+	console.log("sending email to pros", details.results);
+	//var htmlReply = "<p>nearby postcodes</p><div>" + details.postCodes + "</div><p>customer original request</p><div> " + details.customerRequest + " </div>";
+	var htmlReply = "<p>customer original request</p><div> " + details.customerRequest + " </div>";
+	for(var i = details.results.length -1; i >= 0; i--){
+		var recipient = details.results[i].email;		
+		var message = this.createMessage("system@findyour.com.au", "FindYour - customer quote request", recipient, htmlReply);
 		var async = false;
 
 		console.log("sending email to " + recipient);
---
+++ dbManager.js
@@ -0,0 +1,45 @@
+var mongojs = require('mongojs');
+var config = {
+    dbConnStr: 'jndsol:jndsolpass@kahana.mongohq.com:10040/app26850333'
+}
+var db = mongojs(config.dbConnStr);
+var q = require('q');
+
+exports.getDb = function(name){
+	return db;
+}
+
+exports.getConfig = function(){
+	return config;
+}
+
+exports.find = function(col, criteria, returnObj){
+	var deferred = q.defer();
+	var col = db.collection(col);
+	col.find(criteria, function(err, docs) { 
+		if (err) {
+			deferred.reject(new Error("Error: ", err));
+		}
+		if(returnObj){
+			returnObj.results = docs;
+			deferred.resolve(returnObj);
+		}else{
+			deferred.resolve(docs);
+		}
+	});
+	return deferred.promise;
+}
+
+exports.save = function(col, obj){
+	var deferred = q.defer();
+	var col = db.collection(col);
+	col.save(obj, function(err, doc, lastErrorObject) { 
+		if (err) {
+			deferred.reject(new Error("Error: ", err));
+		}
+		deferred.resolve(doc);
+	});
+	return deferred.promise;
+}
+
+	
